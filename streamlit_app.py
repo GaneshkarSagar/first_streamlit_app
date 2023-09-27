8f98ecfbe919ac5c99c1aa173c1e8f2f9c1c892d
GaneshkarSagar <145417652+GaneshkarSagar@users.noreply.github.com>
2023-09-27 19:01:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,4 +21,4 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 # Display the table on the page.
 
 #Choose the fruit name column as index
-
+my_fruit_list = my_fruit_list.set_index
